34b7c8d58ff31f6686547cc6b9c340b1764a7bb2
Yuki Watanabe <hinatabokco@gmail.com>
2022-09-30 08:46:12 +0900

    pdf追加

--- lib/model/person.dart
+++ lib/model/person.dart
@@ -2,17 +2,20 @@ class Person {
   final String id;
   final String name;
   final String ruby;
+  final String initial;
   final String sex;
   final String age;
   final String station;
   final String image;
 
   const Person(
-      {required this.id,
-      required this.name,
-      this.ruby = '',
-      required this.sex,
-      required this.age,
-      required this.station,
-      required this.image});
+      {required this.id, // 社員番号
+      required this.name, // 名前
+      this.ruby = '', // かな
+      required this.initial, // イニシャル
+      required this.sex, // 性別
+      required this.age, // 年齢
+      required this.station, // 駅
+      required this.image // 写真
+      });
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,11 +3,13 @@ import 'package:provider/provider.dart';
 import 'package:resume_app/controller/search_bar_controller.dart';
 import 'package:resume_app/utils/hex_color.dart';
 import 'package:resume_app/screens/search_detail_screen.dart';
+import 'package:resume_app/screens/create_pdf/create_pdf_screen.dart';
 import 'package:banner_listtile/banner_listtile.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchScreen extends StatefulWidget {
   final String searchValue; //上位Widgetから受け取りたいデータ
+
   const SearchScreen({Key? key, required this.searchValue}) : super(key: key);
 
   @override
@@ -16,10 +18,8 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   var color_gray = '757575'; // アイコン、文字色
-
-  var inputWord = '';
+  var _inputWord = '';
   var _selectedSortItem = 'up_ruby';
-
   final _sortMap = {
     'up_ruby': "名前：昇順",
     'down_ruby': "名前：降順",
@@ -27,8 +27,7 @@ class _SearchScreenState extends State<SearchScreen> {
     'down_age': "年齢：降順"
   };
 
-  late String searchValue;
-
+  late String searchValue; // 前画面から受け取った検索用文字列
   @override
   void initState() {
     super.initState();
@@ -80,20 +79,16 @@ class _SearchScreenState extends State<SearchScreen> {
               body: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Center(
-                        // ソート用のドロップダウンリスト
-                        child: _dropDownWidget(searchBarController)),
-                    Flexible(
-                      // 人物リスト
-                      child: _listWidget(context, searchBarController),
-                    ),
+                    // ソート用のドロップダウンリスト
+                    _dropDownWidget(searchBarController),
+                    _listWidget(context, searchBarController),
                   ]));
         },
       ),
     );
   }
 
-  Widget _appbarIcon(SearchBarController searchBarController) {
+  Icon _appbarIcon(SearchBarController searchBarController) {
     return searchBarController.isSearching
         ? Icon(
             Icons.close,
@@ -126,7 +121,7 @@ class _SearchScreenState extends State<SearchScreen> {
             initialValue: searchValue, //ここに初期値
             onChanged: (word) {
               setState(() {
-                inputWord = word;
+                _inputWord = word;
               });
 
               searchBarController.searchOperation(word, _selectedSortItem);
@@ -141,45 +136,47 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   Widget _dropDownWidget(SearchBarController searchBarController) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16, right: 16),
-      child: Container(
-        alignment: Alignment.center, //中央
-        height: 56,
-        child: PopupMenuButton(
-          // elevation: 10,
-          splashRadius: 50,
-          tooltip: 'ソート順[ ${_sortMap[_selectedSortItem] as String} ]',
-          position: PopupMenuPosition.under,
-          iconSize: MediaQuery.of(context).size.width * 0.5,
-          icon: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _searchIcon(_selectedSortItem),
-              Padding(padding: EdgeInsets.only(right: 16)),
-              Text(
-                _sortMap[_selectedSortItem] as String,
-                style: TextStyle(
-                  color: HexColor(color_gray),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16),
+        child: Container(
+          alignment: Alignment.center, //中央
+          height: 56,
+          child: PopupMenuButton(
+            // elevation: 10,
+            splashRadius: 50,
+            tooltip: 'ソート順[ ${_sortMap[_selectedSortItem] as String} ]',
+            position: PopupMenuPosition.under,
+            iconSize: MediaQuery.of(context).size.width * 0.5,
+            icon: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _searchIcon(_selectedSortItem),
+                Padding(padding: EdgeInsets.only(right: 16)),
+                Text(
+                  _sortMap[_selectedSortItem] as String,
+                  style: TextStyle(
+                    color: HexColor(color_gray),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+            itemBuilder: (context) {
+              return _sortMap.entries.map((entry) {
+                return PopupMenuItem(
+                  mouseCursor: MouseCursor.defer,
+                  value: entry.key,
+                  child: Text(entry.value),
+                );
+              }).toList();
+            },
+            onSelected: (newValue) {
+              setState(() {
+                _selectedSortItem = newValue as String;
+              });
+              searchBarController.fetch(_inputWord, newValue as String);
+            },
           ),
-          itemBuilder: (context) {
-            return _sortMap.entries.map((entry) {
-              return PopupMenuItem(
-                mouseCursor: MouseCursor.defer,
-                value: entry.key,
-                child: Text(entry.value),
-              );
-            }).toList();
-          },
-          onSelected: (newValue) {
-            setState(() {
-              _selectedSortItem = newValue as String;
-            });
-            searchBarController.fetch(inputWord, newValue as String);
-          },
         ),
       ),
     );
@@ -206,65 +203,66 @@ class _SearchScreenState extends State<SearchScreen> {
         ? searchBarController.searchedItemList
         : searchBarController.allItemList;
 
-    if (itemList.isEmpty) {
-      return Center(
-        child: Text('"${searchBarController.searchedText}"を含む文字列は見つかりませんでした。'),
-      );
-    } else {
-      return ListView.builder(
-          itemCount: itemList.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: InkWell(
-                child: BannerListTile(
-                  imageContainerShapeZigzagIndex: index,
-                  backgroundColor: Colors.white,
-                  showBanner: false,
-                  bannerPositionRight: false,
-                  borderRadius: BorderRadius.circular(8),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            SearchDatailScreen(itemList[index], index)),
-                  ),
-                  title: Text(
-                    // style: TextStyle(fontSize: 20, color: Colors.white),
-                    itemList[index].name,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  imageContainer: Hero(
-                    tag: 'image${index}',
-                    child: Image.network(
-                      itemList[index].image,
+    return Flexible(
+      child: itemList.isEmpty
+          ? Center(
+              child: Text(
+                  '"${searchBarController.searchedText}"を含む文字列は見つかりませんでした。'),
+            )
+          : ListView.builder(
+              itemCount: itemList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: InkWell(
+                    child: BannerListTile(
+                      imageContainerShapeZigzagIndex: index,
+                      backgroundColor: Colors.white,
+                      showBanner: false,
+                      bannerPositionRight: false,
+                      borderRadius: BorderRadius.circular(8),
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                SearchDatailScreen(itemList[index], index)),
+                      ),
+                      title: Text(
+                        // style: TextStyle(fontSize: 20, color: Colors.white),
+                        itemList[index].name,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      imageContainer: Hero(
+                        tag: 'image${index}',
+                        child: Image.network(
+                          itemList[index].image,
+                        ),
+                      ),
+                      subtitle: Text(
+                        itemList[index].ruby,
+                      ),
+                      trailing: PopupMenuButton(
+                        icon: const Icon(Icons.more_vert),
+                        itemBuilder: (context) {
+                          return [
+                            makePopupMenuItem('edit', FontAwesomeIcons.userPen),
+                            makePopupMenuItem('pdf', FontAwesomeIcons.filePdf),
+                            makePopupMenuItem('delete', FontAwesomeIcons.trash),
+                          ];
+                        },
+                        onSelected: (String value) => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  CreatePdfScreen(person: itemList[index])),
+                        ),
+                      ),
                     ),
                   ),
-                  subtitle: Text(
-                    itemList[index].ruby,
-                    // style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                  trailing: PopupMenuButton(
-                    icon: const Icon(Icons.more_vert),
-                    itemBuilder: (context) {
-                      return [
-                        makePopupMenuItem('edit', FontAwesomeIcons.userPen),
-                        makePopupMenuItem('pdf', FontAwesomeIcons.filePdf),
-                        makePopupMenuItem('delete', FontAwesomeIcons.trash),
-                      ];
-                    },
-                    onSelected: (String value) => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              SearchDatailScreen(itemList[index], index)),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          });
-    }
+                );
+              },
+            ),
+    );
   }
 
   PopupMenuItem<String> makePopupMenuItem(String itemValue, IconData icon) {
